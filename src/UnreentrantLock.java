e087ccae430da7b3d42ce3e2c9807aef454e7cd7
buliwen <buliwen1126@rayootech.com>
2019-02-18 11:33:23 +0800

    update:改造为可重入锁，注释打开即生效

--- src/UnreentrantLock.java
+++ src/UnreentrantLock.java
@@ -8,9 +8,17 @@ import java.util.concurrent.atomic.AtomicReference;
 public class UnreentrantLock {
 
     private AtomicReference<Thread> owner = new AtomicReference<Thread>();
+    //去掉注释即改为可重入锁
+    //private int state = 0;
 
     public void lock() {
         Thread current = Thread.currentThread();
+        //去掉注释即改为可重入锁
+//        if (current == owner.get()) {
+//            state++;
+//            return;
+//        }
+
         //这句是很经典的“自旋”语法，AtomicInteger中也有
         for (;;) {
             if (!owner.compareAndSet(null, current)) {
@@ -21,6 +29,13 @@ public class UnreentrantLock {
 
     public void unlock() {
         Thread current = Thread.currentThread();
-        owner.compareAndSet(current, null);
+        //去掉注释即改为可重入锁
+//        if (current == owner.get()) {
+//            if (state != 0) {
+//                state--;
+//            } else {
+                owner.compareAndSet(current, null);
+//            }
+//        }
     }
 }
